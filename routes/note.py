b80bb6e879a3221bbdfc89aaf3b4b1b78bdab5dc
kishan parkhiya <kishanparkhiya167@gmail.com>
2024-11-22 14:03:07 +0530

    Add : Filters initalized

--- routes/note.py
+++ routes/note.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Query
 from bson import ObjectId
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi import APIRouter
 from modals.note import Note
 from schemas.note import noteEntity,notesEntity
 from config.db import conn
 from fastapi.templating import Jinja2Templates
+import json
+
 
 
 note = APIRouter()
@@ -13,7 +15,7 @@ templates = Jinja2Templates(directory="templates")
 
 @note.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    docs = conn.notes.notes.find({})
+    docs = conn.notes.notes.find({}).sort("is_important", -1)
     newDocs = []
     for doc in docs:
         newDocs.append({
@@ -51,7 +53,6 @@ async def update_note(note_id: str, request: Request):
         except Exception:
             return {"error": "Invalid note_id format", "status_code": 400}
 
-        # Prepare the update query
         update_query = {
             "$set": {
                 "title": formDict.get("title"),
@@ -60,32 +61,51 @@ async def update_note(note_id: str, request: Request):
             }
         }
 
-        # Perform the update in the database
         result = conn.notes.notes.update_one({"_id": note_id}, update_query)
 
-
-        if result.modified_count == 1:
-            return RedirectResponse(url="/", status_code=303)  # Redirect after successful update
-        else:
-            return RedirectResponse(url="/", status_code=303)  # Redirect after successful update
+        return RedirectResponse(url="/", status_code=303)
 
     except Exception as e:
         return {"e" : e}
     
-    
+@note.get("/delete/{note_id}")
+async def delete_note(note_id: str):
+    print("note_id:", note_id)
+    try:
+        note_id_obj = ObjectId(note_id)
+    except Exception:
+        return JSONResponse(
+            content={"error": "Invalid note ID"},
+            status_code=400
+        )
+
+    result = conn.notes.notes.delete_one({"_id": note_id_obj})
+
+    return RedirectResponse(url="/", status_code=303)
+
     
 @note.get("/find_notes/{string}")
 async def find_notes(string: str):
     try:
-        notes_cursor = conn.notes.notes.find({"title": {"$regex": string, "$options": "i"}})
+        notes_cursor = conn.notes.notes.find({"title": {"$regex": string, "$options": "i"}}).sort("is_important", -1)
         notes_list = notesEntity(notes_cursor)
         return notes_list
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
+    
 @note.get("/get_all_notes/")
-async def get_all_notes():
-    docs = conn.notes.notes.find({})
+async def get_all_notes(request: Request, filter: str | None = None):
+    filter_dict = json.loads(filter) if filter else {} 
+    docs = []
+    if "sort" in filter_dict:
+        sort_order = 1 if filter_dict.get('sort', '').upper() == "ASC" else -1
+        docs = conn.notes.notes.find({}).sort("title", sort_order)
+    elif "important" in filter_dict:
+        docs = conn.notes.notes.find({"is_important": filter_dict['important']})
+    else:
+        docs = conn.notes.notes.find({})
+
     newDocs = notesEntity(docs)
     
     return newDocs
--- static/script.js
+++ static/script.js
@@ -1,113 +1,142 @@
+$(document).ready(function () {
 
-// Toggle Read More Section
-function toggleReadMore(id) {
-    const element = document.getElementById(id);
-    element.classList.toggle("hidden");
-}
-
-// Open Modal
-function openModal(id) {
-    const modal = document.getElementById(id);
-    modal.classList.remove("hidden");
-}
-
-// Close Modal
-function closeModal(id) {
-    const modal = document.getElementById(id);
-    modal.classList.add("hidden");
-}
-
-// Close Modal on clicking the background
-window.onclick = function (event) {
-    const modals = document.querySelectorAll(".fixed");
-    modals.forEach((modal) => {
-        if (event.target === modal) {
-            modal.classList.add("hidden");
+    $(".fixed").each(function (event) {
+        if (event.target === this) {
+            $(this).addClass("hidden");
         }
     });
-};
 
-function createNoteHTML(note, index) {
-    const noteElement = document.createElement("div");
-    noteElement.classList.add("relative", "bg-white", "shadow-lg", "rounded-lg", "p-6", "border-t-4");
+    $('#filter').on("change", function () {
+        var filter = $(this).val();
+        var filterObject = {};
+        if (['ASC', 'DESC'].includes(filter)) {
+            filterObject['sort'] = filter;
+        } else if (['IMP', 'NOT_IMP'].includes(filter)) {
+            filterObject['important'] = filter == "IMP" ? true : false;
+        }
 
-    noteElement.classList.add(note.is_important ? "border-red-500" : "border-indigo-500");
+        const url = filterObject && Object.keys(filterObject).length ? `/get_all_notes/?filter=${encodeURIComponent(JSON.stringify(filterObject))}` : `/get_all_notes/`;
 
-    noteElement.innerHTML = `
-        <h6 class="text-lg font-bold text-gray-800">${note.title}</h6>
-        <div class="mt-2">
-            <button class="text-sm text-indigo-600 hover:underline" onclick="toggleReadMore('collapse${index}')">
-                Read note
-            </button>
-            <button class="text-sm text-indigo-600 hover:underline float-right" onclick="openModal('modal${index}')">
-                Edit
-            </button>
-        </div>
-        <div id="collapse${index}" class="hidden mt-4 text-sm text-gray-600">
-            ${note.content}
-        </div>
-        <div id="modal${index}"
-            class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div class="bg-white p-6 rounded-lg shadow-lg w-100">
-                <h5 class="text-lg font-semibold text-gray-700">Edit Note</h5>
-                <form action="/update/${note.id}" method="post" class="mt-4">
-                    <label class="inline-flex items-center mb-2 float-end">
-                        <input type="checkbox" name="is_important" {% if doc.is_important %}checked{% endif
-                            %} class="form-checkbox text-indigo-600" />
-                        <span class="ml-2 text-sm text-gray-600">Important</span>
-                    </label>
-                    <input type="text" name="title" value="${note.title}"
-                        class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300 focus:outline-none" />
-                    <textarea name="content"
-                        class="w-full px-4 py-2 mt-4 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300 focus:outline-none">${note.content}</textarea>
-                    <div class="flex justify-end space-x-2 mt-4">
-                        <button type="button"
-                            class="btn btn-light btn-sm mt-3 pt-1 pb-2 px-3 text-sm rounded-md bg-gray-300 text-gray-800 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500"
-                            onclick="closeModal('modal${index}')">
-                            Cancel
-                        </button>
-                        <button type="submit"
-                            class="btn btn-dark btn-sm mt-3 pt-1 pb-2 px-3 text-sm rounded-md bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
-                            Update
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    `;
+        $.ajax({
+            url: url,
+            method: "GET",
+            dataType: "json",
+            success: function (response) {
+                const notesContainer = $("#notes-container");
+                notesContainer.empty();
 
-    return noteElement;
+                if (response.length === 0) {
+                    notesContainer.html("<span>No matching records found</span>");
+                } else {
+                    notesContainer.append(createNotesHTML(response));
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error("Error fetching notes:", error);
+                const notesContainer = $("#notes-container");
+                notesContainer.html("<span>Error fetching notes. Please try again later.</span>");
+            }
+        });
+    });
+
+});
+function toggleReadMore(id) {
+    $(`#${id}`).toggleClass("hidden");
 }
 
+function openModal(id) {
+    $(`#${id}`).removeClass("hidden");
+}
+
+function closeModal(id) {
+    $(`#${id}`).addClass("hidden");
+}
+
+function createNotesHTML(notes) {
+    const fragment = $(document.createDocumentFragment());
+    notes.forEach((note, index) => {
+        const noteElement = $(`
+            <div class="relative bg-white shadow-lg rounded-lg p-6 border-t-4 ${note.is_important ? "border-red-500" : "border-indigo-500"}">
+                <h6 class="text-lg font-bold text-gray-800">${note.title}</h6>
+                <div class="mt-2">
+                    <button class="text-sm text-indigo-600 hover:underline" onclick="toggleReadMore('collapse${index}')">
+                        Read note
+                    </button>
+                    <button class="text-sm text-red-600 hover:underline float-right" onclick="toggleDeleteModal(true, '${note.id}')">
+                        Delete
+                    </button>
+                    <button class="text-sm text-indigo-600 hover:underline float-right mr-2" onclick="openModal('modal${index}')">
+                        Edit
+                    </button>
+                </div>
+                <div id="collapse${index}" class="hidden mt-4 text-sm text-gray-600">
+                    ${note.content}
+                </div>
+                <div id="modal${index}" class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                    <div class="bg-white p-6 rounded-lg shadow-lg w-100">
+                        <h5 class="text-lg font-semibold text-gray-700">Edit Note</h5>
+                        <form action="/update/${note.id}" method="post" class="mt-4">
+                            <label class="inline-flex items-center mb-2 float-end">
+                                <input type="checkbox" name="is_important" ${note.is_important ? "checked" : ""} class="form-checkbox text-indigo-600" />
+                                <span class="ml-2 text-sm text-gray-600">Important</span>
+                            </label>
+                            <input type="text" name="title" value="${note.title}" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300 focus:outline-none" />
+                            <textarea name="content" class="w-full px-4 py-2 mt-4 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300 focus:outline-none">${note.content}</textarea>
+                            <div class="flex justify-end space-x-2 mt-4">
+                                <button type="button" class="btn btn-light btn-sm mt-3 pt-1 pb-2 px-3 text-sm rounded-md bg-gray-300 text-gray-800 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500" onclick="closeModal('modal${index}')">
+                                    Cancel
+                                </button>
+                                <button type="submit" class="btn btn-dark btn-sm mt-3 pt-1 pb-2 px-3 text-sm rounded-md bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                                    Update
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        `);
+
+        fragment.append(noteElement);
+    });
+
+    return fragment;
+}
 
 async function fetchNotesByTitle() {
     try {
-        const data = document.getElementById('search-input');
+        const data = $('#search-input').val();
+        const url = data ? `/find_notes/${encodeURIComponent(data)}` : `/get_all_notes/`;
+        const response = await $.ajax({
+            url: url,
+            method: "GET",
+            dataType: "json",
+        });
+        console.log("response", response);
 
-        // URL of the FastAPI endpoint
-        const url = data.value ? `/find_notes/${encodeURIComponent(data.value)}` : `/get_all_notes/`;
+        const notesContainer = $("#notes-container");
+        notesContainer.empty();
 
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status} - ${response.statusText}`);
-        }
-        const notes = await response.json();
-        console.log(notes.length);
-        const notesContainer = document.getElementById("notes-container");
-        notesContainer.innerHTML = "";
-        if (notes.length == 0) {
-            notesContainer.innerHTML = "<span>No matching records found</span>";
-
+        if (response.length === 0) {
+            notesContainer.html("<span>No matching records found</span>");
         } else {
-            notes.forEach((note, index) => {
-                const noteElement = createNoteHTML(note, index);
-                notesContainer.appendChild(noteElement);
-            });
+            notesContainer.append(createNotesHTML(response));
         }
-
     } catch (error) {
         console.error("Error fetching notes:", error);
     }
 }
-// fetchNotesByTitle("note")
 
+function toggleDeleteModal(show, noteId) {
+    const modal = $("#deleteModal");
+    const deleteBtn = $("#deleteBtn");
+
+    deleteBtn.off("click").on("click", function () {
+        window.location.href = `/delete/${noteId}`;
+    });
+
+    if (show) {
+        modal.removeClass("hidden");
+    } else {
+        modal.addClass("hidden");
+    }
+}
